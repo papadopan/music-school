61540f9a5a550e05c12ed5a66795cb289a8ac7b3
papadopan <antonios.papadopan@gmail.com>
2021-01-23 00:17:28 +0100

    feat: add tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
     "gatsby-transformer-remark",
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
+    `gatsby-plugin-postcss`,
     "gatsby-plugin-sharp",
     {
       resolve: "gatsby-source-contentful",
